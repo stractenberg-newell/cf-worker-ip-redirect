c9197e99a0dc73bc89f686de3c3e1b44d2ae09d6
Steve Tractenberg <steve.tractenberg@newellco.com>
2021-09-20 14:56:49 -0400

    Changed API auth type

--- deploy_worker.sh
+++ deploy_worker.sh
@@ -1,8 +1,10 @@
-CF_API_TOKEN="<api_token>"
 ACCOUNT_ID="<cf_account_id>"
+CLOUDFLARE_EMAIL="<cf_email_address>"
+CLOUDFLARE_AUTH_KEY="<cf_auth_key>"
 SCRIPT_NAME="<script_name>"
 
 curl -X PUT "https://api.cloudflare.com/client/v4/accounts/$ACCOUNT_ID/workers/scripts/$SCRIPT_NAME" \
-     -H  "Authorization: Bearer $CF_API_TOKEN" \
-     -F "metadata=@metadata.json;type=application/json" \
-     -F "script=@ip_redirect.js;type=application/javascript"
+    -H "X-Auth-Email: $CLOUDFLARE_EMAIL" \
+    -H "X-Auth-Key: $CLOUDFLARE_AUTH_KEY" \
+    -F "metadata=@metadata.json;type=application/json" \
+    -F "script=@ip_redirect.js;type=application/javascript"
